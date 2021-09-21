6c8ca324dd2848f4d6c2259aa2962b28e87e339e
Ingunn-B <ingunn20@ru.is>
2021-09-21 19:52:21 +0000

    tests fix

--- src/test.py
+++ src/test.py
@@ -20,6 +20,15 @@ class TestStationMethods(unittest.TestCase):
         # self.appointment = Appointment()
         # self.staff = Staff()
 
+        self.patient = Patient("0909002020", "Jói Jóason", "Hamraborg 100", "90990909", "joi@gmail.com")
+        self.patient_with_allergy = Patient("1212002320", "Gulla Gull", "Hamraborg 200", "8872233", "gulla@hotmail.com", ["Fish allergy", "Nut allergy"])
+
+        self.doctor = Staff("Anna Önnudóttir", "1010661399", "doctor", "Hamraborg 20", "8992345")
+        self.nurse = Staff("Gunnar Gunnarsson", "0909691399", "nurse", "Hamraborg 10", "7883456")
+
+        self.appoinment_surgery = Appointment(self.patient, [self.doctor, self.nurse], [10,10,2022], "08:00", 120, 2, "Surgery on shoulder.")
+        self.appointment_checkup = Appointment(self.patient, [self.doctor], [10,10,2022], "12:00", 60)
+
 
     def test_prescription_class(self):
 
@@ -34,32 +43,6 @@ class TestStationMethods(unittest.TestCase):
         return_msg = wrapper.send_presription('{"medicine": "Daniel", "pharmecy": "Sara", "patient_id": "Sigur"}')
 
         self.assertEqual(return_msg, '{"medicine": "Daniel", "pharmecy": "Sara", "patient_id": "Sigur"}')
-
-    # #This is a single test case - it runs the reportWeather function in our station
-    # #and makes sure the return value is an empty string
-    # def test_patient_name(self):
-    #     #Run function
-    #     patient_record = self.patient.get_patient("1")
-
-    #     #Assertion
-    #     self.assertEqual(patient_record[0],"Sara")
-    #     self.assertNotEqual(patient_record[1], "21")
-
-    # def test_report_status (self):
-    # #     #Run function
-    #     status = ""#self.my_station.report_status()
-
-    #     #Assertion
-    #     self.assertEqual("",status)
-        # all our tests need an instance of WeatherStation, so we just create one here
-        self.patient = Patient("0909002020", "Jói Jóason", "Hamraborg 100", "90990909", "joi@gmail.com")
-        self.patient_with_allergy = Patient("1212002320", "Gulla Gull", "Hamraborg 200", "8872233", "gulla@hotmail.com", ["Fish allergy", "Nut allergy"])
-
-        self.doctor = Staff("Anna Önnudóttir", "1010661399", "doctor", "Hamraborg 20", "8992345")
-        self.nurse = Staff("Gunnar Gunnarsson", "0909691399", "nurse", "Hamraborg 10", "7883456")
-
-        self.appoinment_surgery = Appointment(self.patient, [self.doctor, self.nurse], [10,10,2022], "08:00", 120, 2, "Surgery on shoulder.")
-        self.appointment_checkup = Appointment(self.patient, [self.doctor], [10,10,2022], "12:00", 60)
 
     # This is a single test case - it runs the reportWeather function in our station
     # and makes sure the return value is an empty string
@@ -118,7 +101,7 @@ class TestStationMethods(unittest.TestCase):
         # ekki komnar upplýsingar um patient út af patient klasa
         doctor = appointment_surgery["staff"][0].get_staff_member()
         nurse = appointment_surgery["staff"][1].get_staff_member()
-        self.assertEquals(len(appointment_surgery["staff"]), 2)
+        self.assertEqual(len(appointment_surgery["staff"]), 2)
         self.assertEqual(doctor["ssn"], "1010661399")
         self.assertEqual(nurse["ssn"], "0909691399")
         self.assertEqual(appointment_surgery["date"], [10,10,2022])
@@ -130,7 +113,7 @@ class TestStationMethods(unittest.TestCase):
         appointment_checkup = self.appointment_checkup.get_info()
         self.assertIsInstance(appointment_checkup, dict)
         doctor = appointment_checkup["staff"][0].get_staff_member()
-        self.assertEquals(len(appointment_checkup["staff"]), 1)
+        self.assertEqual(len(appointment_checkup["staff"]), 1)
         self.assertEqual(doctor["ssn"], "1010661399")
         self.assertEqual(appointment_checkup["date"], [10,10,2022])
         self.assertEqual(appointment_checkup["time"], "12:00")
