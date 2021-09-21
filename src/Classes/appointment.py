c9ce9dc55b9cab9e616b500d191ff5dca450d37f
Ingunn-B <ingunn20@ru.is>
2021-09-21 10:25:24 +0000

    appointment class on the way

--- src/Classes/appointment.py
+++ src/Classes/appointment.py
@@ -1,2 +1,33 @@
+from typing import List
+from Classes.patient import Patient
+from Classes.staff import Staff
+from data import Data
+
+TREATMENTS = {
+    "1": "Regular checkup",
+    "2": "Surgery",
+    "3": "Catscan",
+    "4": "x-rays",
+    "5": "Bloodworks",
+}
+
+
 class Appointment:
-    pass
+    def __init__(
+        self,
+        patient=Patient,
+        list_of_staff=List,
+        date=list,
+        time=int,
+        duration=int,
+        treatment=int,
+    ) -> None:
+        self.patient = patient
+        self.staff_involved = list_of_staff
+        self.date = date
+        self.time = time
+        self.duration = duration
+        if treatment == None:
+            self.treatment = TREATMENTS[1]
+        else:
+            self.treatment = treatment
