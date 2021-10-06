40402d1d1165f77f593fd1d8d6861f1674e01828
Tryggvin <tryggvia20@ru.is>
2021-10-06 13:47:58 +0200

    nurse and doctor classes created, database now has doctor and nurses

--- src/data.py
+++ src/data.py
@@ -2,6 +2,8 @@ from Classes.appointment import Appointment
 from Classes.prescription import Prescription
 from Classes.patient import Patient
 from Classes.staff import Staff
+from Classes.doctor import Doctor
+from Classes.nurse import Nurse
 
 
 prescriptions_list = [
@@ -14,11 +16,25 @@ patients_list = [
     Patient("jonniminn", "Jon Gunnarsson", "jonniminn@tolfan.is", '{"Medication": ["ritalin", "Parkodin", "Astma"]}', "", "")
 ]
 
+#TODO remove staff list
 staff_members = [
     Staff("Johann Johannsson", "1010661399", "doctor", "Hamraborg 20", "8992345"),
     Staff("Gunnar Gunnarsson", "0909691399", "nurse", "Hamraborg 10", "7883456"), 
     Staff("Arna Arnadottir", "0808701399", "specialist", "Hamraborg 30", "5991234"), 
     Staff("Hanna Hannesardottir", "0707891399", "receptionist", "Hamraborg 60", "8566858")]
+
+# username: string, name: string, email: string, note: string, department: string
+doctors = [
+    Doctor("jojo", "Johann Johannsson", "jojo@gmail.com", "professional hamon user", "surgeon"),
+    Doctor("arnaa", "Arna Arnadottir", "arnaa@visir.is", "doctor", "Dermatologist")
+]
+
+# username: string, name: string, email: string, note: string
+nurses = [
+    Nurse("hanna21", "Hanna Hannesardottir", "hanna21@simnet.is"),
+    Nurse("GunGun", "Gunnar Gunnarsson", "GunGun21@gmail.com")
+]
+
 
 appointments = [
     Appointment("icehot", [staff_members[0], staff_members[3]], [10, 8, 2022], "13:00", 60, 2, "Surgery on shoulder"), 
@@ -30,6 +46,14 @@ class Data():
     ''' Our Database Dummy'''
     def __init__(self):
         pass
+    
+    def get_doctors(self):
+        """Returns a list of doctors created to stand in for our database"""
+        return doctors
+    
+    def get_nurses(self):
+        """Returns a list of nurses created to stand in for our database"""
+        return nurses
 
     def get_patients(self):
         """Returns a list of patients created to stand in for our database"""
